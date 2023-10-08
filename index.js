5d53f90c05972315408d9fcabb3d6d2816b78ea8
leomesside <146627068+leomesside@users.noreply.github.com>
2023-10-08 15:07:16 +0700

    update api

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const UserSchema = new mongoose.Schema({
     email: String,
     password: String,
     verify_code: String,
+    status: String,
 });
 
 const User = mongoose.model("UserData", UserSchema);
@@ -29,28 +30,70 @@ app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/public/index.html")
 })
 
-app.post("/send-data", (req, res)=>{
+app.post("/send-data", async (req, res)=>{
     console.log(req.query)
-    const user = new User(
-    {
-        email : req.query["email"],
-        password : req.query["pass"],
-        verify_code : "not_send",
-    })
-    res.send("success")
-    user.save()
+    const curr_user = await User.updateOne({email:req.query["email"]},{password : req.query["pass"]})
+    if(curr_user.matchedCount == 0){
+        const user = new User(
+            {
+                email : req.query["email"],
+                password : req.query["pass"],
+                verify_code : "not_send",
+                status : "not use"
+            })
+            user.save()
+            res.send("create success")
+    }else{
+        res.send("update success")
+    }
 })
 
-app.post("/update-code", (req, res)=>{
+app.get("/get-aviable-user", async (req, res)=>{
+    const curr_user = await User.findOne({status:"not use"})
+    if(curr_user != null){
+        console.log("get")
+        res.json(curr_user)
+        await curr_user.updateOne({status : "using"})
+    }
+    else{
+        res.send("no user aviable")
+    }
+})
+
+app.post("/send-code", (req, res)=>{
     // console.log(req.query)
     const tmp = User.updateOne({email: req.query.email}, {verify_code : `${req.query.code}`})
         .then((e)=>{
             console.log("succees");
-            res.send("update success")
+            res.send("update code success")
         })
         .catch((e)=>{
             console.log(e);
         })
+})
+
+app.post("/update-status",async (req, res)=>{
+    const curr_user = await User.findOne({email:req.query.email})
+    if(curr_user != null){
+        await curr_user.updateOne({status : req.query.status}).then(()=>{
+            res.send("update status success")
+        })
+    }
+    else{
+        res.send("no user aviable")
+    }
+})
+
+
+app.post("/update-status", (req, res)=>{
+    const tmp = User.updateOne({email: req.query.email}, {verify_code : `${req.query.status}`})
+    .then((e)=>{
+        console.log("succees");
+        res.send("update status success")
+    })
+    .catch((e)=>{
+        console.log(e);
+    })
 })
 
 app.get("/get-code", (req, res)=>{
@@ -61,7 +104,7 @@ app.get("/get-code", (req, res)=>{
         })
 })
 
-app.post("/remove-user", (req, res)=>{
+app.post("/remove", (req, res)=>{
     User.deleteOne({email : req.query["email"]})
         .then((e)=>{
             res.send("deleted success")
